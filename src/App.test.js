80be21c20587e2ee6e2c6077b2808733db350b5e
sureshtaninki <113276788+sureshtaninki@users.noreply.github.com>
2023-01-12 12:46:17 +0530

    Delete src directory

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Weather from './App';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<Weather/>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
